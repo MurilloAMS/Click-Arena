39f57e22d5cea5d10c166fc57b86c9f14e75f603
Murillo <mumoura9@gmail.com>
2026-05-02 10:23:26 -0300

    fix wildcard definitivo

--- backend/server.js
+++ backend/server.js
@@ -188,7 +188,7 @@ const path = require("path");
 
 app.use(express.static(path.join(__dirname, "../frontend")));
 
-app.get("/", (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/index.html"));
 });
 
