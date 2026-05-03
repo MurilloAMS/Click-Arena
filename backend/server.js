253f2d799f2354331325cf70c6f033b22360f6e9
Murillo <mumoura9@gmail.com>
2026-05-03 12:11:56 -0300

    teste sem mercado pago

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,18 @@
 const express = require("express");
 const cors = require("cors");
-const { MercadoPagoConfig, Payment } = require("mercadopago");
+//const { MercadoPagoConfig, Payment } = require("mercadopago");
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 let jogadores = {};
 
-const client = new MercadoPagoConfig({
-  accessToken: "siUM0YNkFaqAknXaIHc39kbgW5dXf11R"
-});
+console.log("NOVA VERSAO RODANDO");
+//const client = new MercadoPagoConfig({
+  //accessToken: "siUM0YNkFaqAknXaIHc39kbgW5dXf11R"
+//});
 
-const payment = new Payment(client);
+//const payment = new Payment(client);
 
 // 💾 "banco fake"
 let usuarios = {};
@@ -23,67 +24,67 @@ let saques = [];
 // =============================
 // 💰 CRIAR PAGAMENTO (PIX)
 // =============================
-app.post("/criar-pagamento", async (req, res) => {
-  const { valor, userId } = req.body;
+//app.post("/criar-pagamento", async (req, res) => {
+//  const { valor, userId } = req.body;
 
-  try {
-    const pagamento = await payment.create({
-      body: {
-        transaction_amount: Number(valor),
-        description: "Deposito Click Arena",
-        payment_method_id: "pix",
+//  try {
+   // const pagamento = await payment.create({
+   //   body: {
+   //     transaction_amount: Number(valor),
+   //     description: "Deposito Click Arena",
+   //     payment_method_id: "pix",
 
-        payer: {
-          email: "usuario@seudominio.com"
-        },
+  //      payer: {
+   //       email: "usuario@seudominio.com"
+   //     },
 
-        external_reference: userId,
+    //    external_reference: userId,
 
-        notification_url: "https://SEU-SITE.com/webhook"
-      }
-    });
+   //     notification_url: "https://SEU-SITE.com/webhook"
+   //   }
+    //});
 
-    res.json({
-      id: pagamento.id,
-      qr_code: pagamento.point_of_interaction.transaction_data.qr_code,
-      qr_base64: pagamento.point_of_interaction.transaction_data.qr_code_base64
-    });
+   // res.json({
+   //   id: pagamento.id,
+   //   qr_code: pagamento.point_of_interaction.transaction_data.qr_code,
+  //    qr_base64: pagamento.point_of_interaction.transaction_data.qr_code_base64
+  //  });
 
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ erro: err.message });
-  }
-});
+ // } catch (err) {
+  //  console.log(err);
+ //   res.status(500).json({ erro: err.message });
+ // }
+//});
 
 // =============================
 // 🔔 WEBHOOK (CONFIRMA PAGAMENTO)
 // =============================
-app.post("/webhook", async (req, res) => {
-  try {
-    const data = req.body;
+//app.post("/webhook", async (req, res) => {
+ // try {
+ //   const data = req.body;
 
-    if (data.type === "payment") {
-      const paymentId = data.data.id;
+  //  if (data.type === "payment") {
+  //    const paymentId = data.data.id;
 
-      const pagamento = await payment.get({ id: paymentId });
+ //     const pagamento = await payment.get({ id: paymentId });
 
-      if (pagamento.status === "approved") {
-        const userId = pagamento.external_reference;
-        const valor = pagamento.transaction_amount;
+   //   if (pagamento.status === "approved") {
+    //    const userId = pagamento.external_reference;
+   //     const valor = pagamento.transaction_amount;
 
-        console.log("💰 PAGAMENTO APROVADO:", userId, valor);
+   //     console.log("💰 PAGAMENTO APROVADO:", userId, valor);
 
         // 👉 AQUI VOCÊ ATUALIZA O SALDO DO USUÁRIO
-      }
-    }
+   //   }
+  //  }
 
-    res.sendStatus(200);
+  //  res.sendStatus(200);
 
-  } catch (err) {
-    console.log("Erro webhook:", err);
-    res.sendStatus(500);
-  }
-});
+ // } catch (err) {
+  //  console.log("Erro webhook:", err);
+  //  res.sendStatus(500);
+//  }
+//});
 
 // =============================
 // 💸 SACAR DINHEIRO
